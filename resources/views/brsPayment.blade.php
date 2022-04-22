c0c42f191e34b232eb4526f12deacda5f6402d1b
Jay Gicanal <gicanaljay24@gmail.com>
2022-04-23 01:08:34 +0800

    toto

--- resources/views/brsPayment.blade.php
+++ resources/views/brsPayment.blade.php
@@ -9,17 +9,17 @@
 
     <section class="payment">
         <div class="container">
-            <div class="row d-flex justify-content-center align-items-center">
+            <div class="row d-flex justify-content-center">
                 <div class="col-md-7 payment-content">
                     <div class="payment-head">
                         <h2>Payment</h2>
                     </div>
                     <nav>
                         <div class="payment-inner">
-                            <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                                <button class="nav-link active" id="nav-Paymaya-tab" data-bs-toggle="tab" data-bs-target="#nav-Paymaya" type="button" role="tab" aria-controls="nav-Paymaya" aria-selected="true">Paymaya</button>
-                                <button class="nav-link" id="nav-Palawan-Express-tab" data-bs-toggle="tab" data-bs-target="#nav-Palawan-Express" type="button" role="tab" aria-controls="nav-Palawan-Express" aria-selected="false">Palawan Express</button>
-                                <button class="nav-link" id="nav-Gcash-tab" data-bs-toggle="tab" data-bs-target="#nav-Gcash" type="button" role="tab" aria-controls="nav-Gcash" aria-selected="false">Gcash</button>
+                            <div class="nav nav-tabs col-12-md" id="nav-tab" role="tablist">
+                                    <button class="nav-link active col-4" id="nav-Gcash-tab" data-bs-toggle="tab" data-bs-target="#nav-Gcash" type="button" role="tab" aria-controls="nav-Gcash" aria-selected="false"> <img src="{{ asset('images/gcash.png') }}" alt=""></button>
+                                    <button class="nav-link col-4" id="nav-Palawan-Express-tab" data-bs-toggle="tab" data-bs-target="#nav-Palawan-Express" type="button" role="tab" aria-controls="nav-Palawan-Express" aria-selected="false"><img src="{{ asset('images/palawan.jpg') }}" alt=""></button>
+                                    <button class="nav-link col-4" id="nav-Paymaya-tab" data-bs-toggle="tab" data-bs-target="#nav-Paymaya" type="button" role="tab" aria-controls="nav-Paymaya" aria-selected="true"> <img src="{{ asset('images/paymaya.png') }}" alt=""> </button>
                             </div>
                         </div>
                     </nav>
@@ -29,8 +29,7 @@
                         <div class="tab-pane fade" id="nav-Gcash" aria-labelledby="nav-Gcash-tab">Gcash</div>
                     </div>
                 </div>
-                <div class="col-md-5  payment-content">
-                   
+                <div class="col-md-5  payment-side">
                 </div>
             </div>
         </div>
